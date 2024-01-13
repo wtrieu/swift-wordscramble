0ce1ff66053601ad0c3a14ef455d741e2428028f
wtrieu <waylantrieu@gmail.com>
2024-01-13 09:37:58 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Waylan Trieu on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
